925b93def0178b5a092f0ac966a9c2f8c9c50d2d
MrDuck <aghakoeswantoro@gmail.com>
2025-12-04 19:59:01 +0800

    Extracted input mapping into its own hashmap-like class

--- src/main/java/com/comp2042/GameViewModel.java
+++ src/main/java/com/comp2042/GameViewModel.java
@@ -1,35 +1,19 @@
 package com.comp2042;
 
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.KeyCode;
 
 public class GameViewModel implements ViewModel {
     private final InputEventListener eventListener;
+    private final BaseInputMap inputMap;
 
     public GameViewModel(InputEventListener eventListener){
         this.eventListener = eventListener;
+        this.inputMap = new GameInputMap();
     }
 
     @Override
     public void handleKey(KeyEvent keyEvent) {
-        if (keyEvent.getCode() == KeyCode.LEFT || keyEvent.getCode() == KeyCode.A) {
-            eventListener.handleEvent(EventType.MOVE_LEFT);
-        }
-        else if (keyEvent.getCode() == KeyCode.RIGHT || keyEvent.getCode() == KeyCode.D) {
-            eventListener.handleEvent(EventType.MOVE_RIGHT);
-        }
-        else if (keyEvent.getCode() == KeyCode.UP || keyEvent.getCode() == KeyCode.W) {
-            eventListener.handleEvent(EventType.MOVE_ROTATE);
-        }
-        else if (keyEvent.getCode() == KeyCode.DOWN || keyEvent.getCode() == KeyCode.S) {
-            eventListener.handleEvent(EventType.MOVE_DOWN);
-        }
-        else if (keyEvent.getCode() == KeyCode.P) {
-            eventListener.handleEvent(EventType.PAUSE);
-        }
-        else if (keyEvent.getCode() == KeyCode.N) {
-            eventListener.handleEvent(EventType.NEW_GAME);
-        }
+        eventListener.handleEvent(inputMap.get(keyEvent.getCode()));
         keyEvent.consume();
     }
 }
